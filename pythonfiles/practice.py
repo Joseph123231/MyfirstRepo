57fb2905caef6ead27ba6567c56ae129157f4fab
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-11 17:00:03 -0400

    Ask teacehr about no iput and letters as input

--- pythonfiles/practice.py
+++ pythonfiles/practice.py
@@ -1,9 +1,15 @@
-age = int(input())
-if age >= 18:
-    print('You can vote')
-if age < 0:
-    print('You are a time traveller')
-if age < 18 and age >= 0:
-    print('You are too young')
+import math
+variable1 = float(input("what's the radius of your circle "))
+area = variable1**2 * math.pi
+circumfrence = 2*math.pi*variable1
+if variable1 > 0:
+    print(" The area of your circle is ", area , " and the circumfrence is ", circumfrence,".")
+if variable1 < 0:
+    print("Please enter a number that is POSITIVE")
+if variable1 == 0:
+    print("Please use a number other then ZERO") 
+if variable1 == None:
+    print("Please enter a positive number")
+    ### Need to make a scenario for input = nothing and input = letters ###
 
 
